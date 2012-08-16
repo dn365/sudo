85d76b91fcf25b2012e53d91ba76c638d1167b9a
dn365 <duning3650@gmail.com>
2012-08-16 10:03:36 +0800

    file exist fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -30,18 +30,18 @@ when "hpux"
     if node["cpu"]["0"]["model"] == "Itanium"
       execute "sudo" do
         command "/usr/sbin/swinstall -s /var/chef/pkg/sudo-1.8.5p2-ia64-11.31.depot sudo"
-        not_if { File.exists?("/usr/local/bin/sudo")}
+        not_if { File.exist?("/usr/local/bin/sudo")}
       end
     elsif node["cpu"]["0"]["model"].include?("PA RISC")
       execute "sudo" do
         command "/usr/sbin/swinstall -s /var/chef/pkg/sudo-1.8.5p2-hppa-11.31.depot sudo"
-        not_if { File.exists?("/usr/local/bin/sudo")}
+        not_if { File.exist?("/usr/local/bin/sudo")}
       end
     end
   elsif node.os_version.include?("11.11")
     execute "sudo" do
       command "/usr/sbin/swinstall -s /var/chef/pkg/sudo-1.8.5p2-hppa-11.11.depot sudo"
-      not_if { File.exists?("/usr/local/bin/sudo")}
+      not_if { File.exist?("/usr/local/bin/sudo")}
     end
   end
 end
